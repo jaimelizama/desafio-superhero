83f87b6ef17fb83300fa34e32f049d857ae0d934
Jaime Lizama R <jlizama@poptimize.cl>
2021-07-16 23:55:52 -0400

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,101 @@
+const formElement = $("#form");
+const inputElement = $("#superhero-input");
+console.log(formElement);
+
+formElement.submit(function (event) {
+  event.preventDefault();
+  console.log("submit");
+  const idSuperhero = inputElement.val();
+
+  $.ajax({
+    type: "GET",
+    datatype: "json",
+    url: `https://www.superheroapi.com/api.php/10158423431493063/${idSuperhero}`,
+  }).done(function (superhero) {
+    console.log(superhero);
+    console.log(superhero.response);
+    if (superhero.response === "error") {
+      alert(
+        "El número ingresado no arrojó ningún resultado.\nFavor intente nuevamente con otro número :)"
+      );
+    } else {
+      //$("#result").text(superhero.name); // Probando el resultado
+      $("#main-content").show();
+      let heroImage = superhero.image.url;
+      $("#hero-image").attr("src", superhero.image.url);
+      $("#hero-name").text(superhero.name);
+      $("#hero-connections").text(superhero.connections["group-affiliation"]);
+      $("#hero-publisher").text(superhero.biography.publisher);
+      $("#hero-occupation").text(superhero.work.occupation);
+      $("#hero-first-appearance").text(superhero.biography["first-appearance"]);
+      $("#hero-height-inch").text(superhero.appearance.height[0]);
+      $("#hero-height-cm").text(superhero.appearance.height[1]);
+      //superhero.appearance.height.forEach((element) => {
+      // $("#hero-height").append(`${element}`);
+      // console.log(element);
+      // });
+      $("#hero-weight-lb").text(superhero.appearance.weight[0]);
+      $("#hero-weight-kg").text(superhero.appearance.weight[1]);
+      $("#hero-aliases").text(superhero.biography.aliases.join(" - "));
+      // superhero.biography.aliases.forEach((aliase) => {
+      //   $("#hero-aliases").append(`${aliase} `);
+      // });
+
+      // chartJS DataPoints
+
+      console.log(superhero.powerstats);
+
+      let powerstatsKeys = Object.keys(superhero.powerstats);
+      console.log(powerstatsKeys);
+
+      let powerstatsValues = Object.values(superhero.powerstats);
+      console.log(powerstatsValues);
+
+      //ChartJS starts
+      document.querySelector("#chartContainer").innerHTML =
+        '<canvas id="powerstatsChart"></canvas>';
+
+      var ctx = document.getElementById("powerstatsChart").getContext("2d");
+      var myChart = new Chart(ctx, {
+        type: "radar",
+        data: {
+          labels: powerstatsKeys,
+          datasets: [
+            {
+              label: `Puntaje`,
+              data: powerstatsValues,
+              backgroundColor: "rgba(255, 99, 132, 0.2)",
+              borderColor: "rgba(255, 99, 132, 1)",
+              borderWidth: 2,
+            },
+          ],
+        },
+        options: {
+          // scales: {
+          //   y: {
+          //     beginAtZero: true,
+          //   },
+          // },
+          plugins: {
+            title: {
+              display: true,
+              font: {
+                size: 18,
+              },
+              text: `Estadísticas de Poder para ${superhero.name}`,
+            },
+            legend: {
+              labels: {
+                font: {
+                  size: 14,
+                },
+                padding: 5,
+              },
+            },
+          },
+        },
+      });
+      //ChartJS ends
+    }
+  });
+});
